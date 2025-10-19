8f5ed12f57966897c2749dd29d620d35de6b7625
centoano-818 <adrian.nowak.star@gmail.com>
2025-10-19 22:56:26 +0200

    fix errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
   integrations: [tailwind()],
   site: 'https://github.com/centano-818',
   base: '/portfolio',
+  output: "static"
 });
